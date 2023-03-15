d40e98584620d0de16a7fc5ecc5b4ca97d57e256
DanielAGrajales23 <dgrajales690@gmail.com>
2023-03-15 16:19:52 -0500

    Modificar html y javascript 2.0

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,27 @@
+let Nombre, Correo , Mensaje;
 
+let Formulario = document.getElementById("Form")
+
+Formulario.addEventListener("Summit", (e)=>{
+    e.preventDefault()
+})
+function leeDatos(){
+    Nombre = document.getElementById("Nombre").Value
+    Correo = document.getElementById("Correo").Value
+    Mensaje = document.getElementById("Textarea").Value
+}
+Formulario.addEventListener("Summit", (e)=>{
+    e.preventDefault()
+    leeDatos()
+})
+function ValidarData(Nombre,Correo,Textarea){
+    if(Nombre.length==0 || Correo.length==0 || Mensaje.length==0){
+        Swal.fire({
+            Title: "Error!",
+            Text:  "Do you want to continue",
+            icon:  "erorr",
+            confirmButtonText: "Cool",
+            iconColor: "Purple"
+        })  
+    }
+}
